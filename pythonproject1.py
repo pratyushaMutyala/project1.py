a8818d05bea4ca2aafed2b6ad6c320bf4423f158
pratyushaMutyala <129381270+pratyushaMutyala@users.noreply.github.com>
2023-04-22 15:09:15 +0530

    Create pythonproject1.py

---
+++ pythonproject1.py
@@ -0,0 +1,3 @@
+print ("omnamashivaya all good things should happen to everyone, please give me strength for everything.")
+prints ("thankyou god for this beautiful life you have given")
+print ("thankyou")
