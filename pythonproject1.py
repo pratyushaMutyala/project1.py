fda35349ab28c2c722606dabf1b5ba2f09874ac3
pratyushaMutyala <129381270+pratyushaMutyala@users.noreply.github.com>
2023-05-31 15:44:39 +0530

    Update pythonproject1.py

--- pythonproject1.py
+++ pythonproject1.py
@@ -1,3 +1,10 @@
-print ("omnamashivaya all good things should happen to everyone, please give me strength for everything.")
-prints ("thankyou god for this beautiful life you have given")
-print ("thankyou")
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8 
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
